9657d2253bfca8d69eef05ca545ea9c57449006f
邱晨龙 <601976246@qq.com>
2019-07-01 21:57:59 +0800

    Update BookController.class.php

--- book/App/Home/Controller/BookController.class.php
+++ book/App/Home/Controller/BookController.class.php
@@ -843,7 +843,7 @@ $data = $Model->Query("SELECT saleprice FROM jieqi_article_article where article
        {
      $where=$where." and  t.articletype =".$_REQUEST['articletype']."";
        }
-       if($_REQUEST['articletype'])//是否已完结
+       if($_REQUEST['typeid'])//是否已完结
        {
      $where=$where." and  t.typeid =".$_REQUEST['typeid']."";
        }
